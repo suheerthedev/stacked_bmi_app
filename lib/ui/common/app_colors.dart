7ebb88f06cbcc9a48ceb68306f0d997ca854aac3
Suheer Khan <khansuheer1@gmail.com>
2025-02-21 15:41:20 +0500

    Removed default home view code, added google fonts package, and added app colors in helper file

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-const Color kcPrimaryColor = Color(0xFF9600FF);
+const Color wTBackgroundColor = Color(0xFFD1D9E6);
+const Color wTPrimaryColor = Color(0xFF246AFE);
+const Color wTSecondaryColor = Color(0xFFFFFFFF);
+
+//Text Color
+const Color wTPrimaryTextColor = Color(0xFF000000);
+const Color wTSecondaryTextColor = Color(0xFFFFFFFF);
+const Color wTThemeTextColor = Color(0xFF246AFE);
+const Color wTLightTextColor = Color(0xFF8C8C8C);
+
 const Color kcPrimaryColorDark = Color(0xFF300151);
 const Color kcDarkGreyColor = Color(0xFF1A1B1E);
 const Color kcMediumGrey = Color(0xFF474A54);
--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -20,55 +20,10 @@ class HomeView extends StackedView<HomeViewModel> {
           padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Center(
             child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                verticalSpaceLarge,
-                Column(
-                  children: [
-                    const Text(
-                      'Hello, STACKED!',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    verticalSpaceMedium,
-                    MaterialButton(
-                      color: Colors.black,
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showDialog,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showBottomSheet,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                )
+                Text("Welcome"),
+                Text("BMI Calculator"),
               ],
             ),
           ),
