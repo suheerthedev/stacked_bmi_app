20a7cd1514ef19bd3831a2f8dd16671a55d79ef9
Suheer Khan <khansuheer1@gmail.com>
2025-02-21 22:14:37 +0500

    Created Result View

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,13 +4,15 @@ import 'package:stacked_bmi_app/ui/views/home/home_view.dart';
 import 'package:stacked_bmi_app/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_bmi_app/ui/views/result/result_view.dart';
 // @stacked-import
 
 @StackedApp(
   routes: [
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
-    // @stacked-route
+    MaterialRoute(page: ResultView),
+// @stacked-route
   ],
   dependencies: [
     LazySingleton(classType: BottomSheetService),
