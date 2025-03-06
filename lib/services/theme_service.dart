f393597f7de612a51ca6643736bd66d275bb447d
Suheer Khan <khansuheer1@gmail.com>
2025-03-07 00:23:50 +0500

    Updated Theme Service

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -1,1 +1,20 @@
-class ThemeService {}
+import 'package:stacked/stacked.dart';
+
+class ThemeService with ListenableServiceMixin {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeService() {
+    loadTheme();
+  }
+
+  Future<void> loadTheme() async {
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
