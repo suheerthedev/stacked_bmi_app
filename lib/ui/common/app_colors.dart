e1c7dedb59d453805b9a4f0f99ec1ac237277e9c
Suheer Khan <khansuheer1@gmail.com>
2025-03-07 00:39:10 +0500

    Added all the colors in app colors file

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:stacked_bmi_app/app/app.locator.dart';
+import 'package:stacked_bmi_app/services/theme_service.dart';
+
+final ThemeService themeServie = locator<ThemeService>();
+
+class AppColors {
+  static Color get background => themeServie.isDarkMode
+      ? const Color(0xFF242424)
+      : const Color(0xFFD1D9E6);
+  static Color get mainColor => themeServie.isDarkMode
+      ? const Color(0xFF373737)
+      : const Color(0xFFFFFFFF);
+  static Color get primaryColor => themeServie.isDarkMode
+      ? const Color(0xFF246AFE)
+      : const Color(0xFF246AFE);
+  static Color get mainTextColor => themeServie.isDarkMode
+      ? const Color(0xFFFFFFFF)
+      : const Color(0xFF000000);
+  static Color get themeTextColor => themeServie.isDarkMode
+      ? const Color(0xFF246AFE)
+      : const Color(0xFF246AFE);
+  static Color get lightTextColor => themeServie.isDarkMode
+      ? const Color(0xFF8C8C8C)
+      : const Color(0xFF8C8C8C);
+}
 
 const Color wTBackgroundColor = Color(0xFFD1D9E6);
 const Color wTPrimaryColor = Color(0xFF246AFE);
